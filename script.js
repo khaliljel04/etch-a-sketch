1a1c1fa8dfd28e654421addab687706b80f7dd11
Khalil <111660184+khaliljel04@users.noreply.github.com>
2023-02-03 22:42:04 +0100

    Add different brushes and more polish

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const button = document.querySelector('#size');
+button.addEventListener('click', () => {
+    const side = prompt('How many squares per side?\nA value between 10 and 60 is recommended');
+    resetBoard();
+    setUpBoard(side);
+});
+
 function setUpBoard(side) {
     const board = document.querySelector('#board');
     for (let i = 0; i < side; i++) {
@@ -14,7 +21,7 @@ function setUpBoard(side) {
 
 function setUpPixel(pixel) {
     pixel.classList.add('pixel');
-    pixel.addEventListener('mouseover', () => pixel.classList.add('hover'));
+    pixel.addEventListener('mouseover', () => colorPixel(pixel));
 }
 
 function resetBoard() {
@@ -22,9 +29,55 @@ function resetBoard() {
     board.innerHTML = '';
 }
 
-const button = document.querySelector('#size');
-button.addEventListener('click', () => {
-    const side = prompt('How many squares per side?\nA value between 10 and 60 is recommended');
-    resetBoard();
-    setUpBoard(side);
-});
+function getBrush() {
+    const brush = document.getElementById('brush');
+    const selectedBrush = brush[brush.selectedIndex].value;
+    return selectedBrush;
+}
+
+function colorPixel(pixel) {
+    switch (getBrush()) {
+        case 'white':
+            colorWhite(pixel);
+            break;
+
+        case 'random':
+            colorRandom(pixel);
+            break;
+
+        case 'grayscale':
+            colorGrayscale(pixel);
+            break;
+    
+        default:
+            colorBlack(pixel);
+            break;
+    }
+}
+
+function colorBlack(pixel) {
+    pixel.style.backgroundColor = 'black';
+}
+
+function colorWhite(pixel) {
+    pixel.style.backgroundColor = 'white';
+}
+
+function colorGrayscale(pixel) {
+    const computedStyles = window.getComputedStyle(pixel);
+
+    // 'rgb(r, g, b)'
+    let backgroundColor = computedStyles.getPropertyValue("background-color");
+    
+    // [r, g, b]
+    backgroundColor = backgroundColor.slice(4,-1).split(',');
+
+    let r = backgroundColor[0].valueOf() - 32;
+    let g = backgroundColor[1].valueOf() - 32;
+    let b = backgroundColor[2].valueOf() - 32;
+    pixel.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function colorRandom(pixel) {
+    pixel.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+}
